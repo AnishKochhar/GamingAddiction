88abc63c144ee76d95781ed8384d831d98904822
Anish Kochhar <anishk12@icloud.com>
2020-06-06 14:38:26 +0100

    Finished "Add Device" and "Add Device Type" in the add session view

--- Gaming Addiction/AddSessionViewController.swift
+++ Gaming Addiction/AddSessionViewController.swift
@@ -12,12 +12,24 @@ class AddSessionViewController: UIViewController {
     
     // MARK: Properties
     // This array stores all of the user's devices
-    var devices = ["Add Device"]
-    // Store the currently selected device. By default this is "Add Device"
-    var selectedDevice = "Add Device"
+    let devices = ["Xbox", "Play Station", "Nintendo Wii", "Nintendo Wii U", "Nintendo DS", "PSP Vita",
+                    "iOS device", "Android Device",
+                    "PC", "Laptop"]
+    // Array of device types
+    let deviceTypes = ["Console", "Mobile", "Computer", "Other"]
+    // Store the currently selected device. By default this is "Xbox"
+    var selectedDevice = ""
+    var selectedDeviceType = ""
+    // Create the hours and minutes array, for the time select picker view
+    let hours = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "10+"]
+    let minutes = Array(0...60)
+    
+    let devicePicker: UIPickerView = UIPickerView()
+    let deviceTypePicker: UIPickerView = UIPickerView()
 
     @IBOutlet weak var deviceTextField: UITextField!
     @IBOutlet weak var deviceTypeTextField: UITextField!
+    @IBOutlet weak var timeTextField: UITextField!
     
     
     override func viewDidLoad() {
@@ -26,76 +38,47 @@ class AddSessionViewController: UIViewController {
         // Create the pickers and make them the text field's input view.
         createPickers()
         // Add the toolbars to the text views
-        createDeviceToolbar()
-        createDeviceTypeToolbar()
+        createToolbar()
+        print(minutes)
     }
     
     func createPickers() {
-        // Initialise Picker, and assign delegate to self
-        let devicePicker = UIPickerView()
+        // Assign picker's delegate to self
         devicePicker.delegate = self
         // Set the text field's input view to the picker
         deviceTextField.inputView = devicePicker
         
-        let deviceTypePicker = UIPickerView()
         deviceTypePicker.delegate = self
-        
         deviceTypeTextField.inputView = deviceTypePicker
     }
     
-    func createDeviceToolbar() {
+    func createToolbar() {
         // Create a UI toolbar so users can dismiss the picker view
-        let deviceToolbar = UIToolbar()
-        deviceToolbar.sizeToFit()
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
         
         // Create a done button so they can leave the picker view.
-        let doneButton = UIBarButtonItem(title: "Select", style: .plain, target: self, action: #selector(AddSessionViewController.dismissKeyboardAndChangeView))
+        let doneButton = UIBarButtonItem(title: "Select", style: .plain, target: self, action: #selector(AddSessionViewController.dismissKeyboard))
         // Call the dismissKeyboard functiom
         
         // Add the button to the toolbar
-        deviceToolbar.setItems([doneButton], animated: false)
-        deviceToolbar.isUserInteractionEnabled = true
+        toolbar.setItems([doneButton], animated: false)
+        toolbar.isUserInteractionEnabled = true
         
         
-        deviceTextField.inputAccessoryView = deviceToolbar
-        
-     
+        deviceTextField.inputAccessoryView = toolbar
+        deviceTypeTextField.inputAccessoryView = toolbar
     }
     
-    func createDeviceTypeToolbar() {
-         // Create a UI toolbar so users can dismiss the picker view
-         let deviceTypeToolbar = UIToolbar()
-         deviceTypeToolbar.sizeToFit()
-         
-         // Create a done button so they can leave the picker view.
-         let doneButton = UIBarButtonItem(title: "Select", style: .plain, target: self, action: #selector(AddSessionViewController.dismissKeyboard))
-         // Call the dismissKeyboard functiom
-         
-         // Add the button to the toolbar
-         deviceTypeToolbar.setItems([doneButton], animated: false)
-         deviceTypeToolbar.isUserInteractionEnabled = true
-         
-         
-         deviceTypeTextField.inputAccessoryView = deviceTypeToolbar
-    }
-    
-    // This is so a text view can be shown if the user wishes to add a device
-    @objc func dismissKeyboardAndChangeView() {
-        // Clear the picker view
-        view.endEditing(true)
-
-        if selectedDevice == "Add Device" {
-            // The user has chosen to add a device
-            deviceTextField.placeholder = "Click here to type"
-            // nil is the default value of inputView, so changing it back to nil loads the text field normally
-            deviceTextField.inputView = nil
-            deviceTextField.delegate = self
-            
-            deviceTypeTextField.inputAccessoryView = nil
-        }
-    }
-    // This is for the device type selection, which doesn't contain a text view.
+    // Dismisses the picker view
     @objc func dismissKeyboard() {
+        // The user has changed the picker view, then change the UI
+        if selectedDevice != "" {
+            deviceTextField.text = devices[devicePicker.selectedRow(inComponent: 0)]
+        }
+        if selectedDeviceType != "" {
+            deviceTypeTextField.text = deviceTypes[deviceTypePicker.selectedRow(inComponent: 0)]
+        }
         view.endEditing(true)
     }
 }
@@ -106,23 +89,32 @@ extension AddSessionViewController: UIPickerViewDataSource, UIPickerViewDelegate
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return devices.count
+        if pickerView == devicePicker {
+            // As soon as they open the picker view, the text field will be able to change (change dismissKeyboard method)
+            selectedDevice = "test"
+            return devices.count
+        } else {
+            selectedDeviceType = "test"
+            return deviceTypes.count
+        }
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return devices[row]
+        if pickerView == devicePicker {
+            return devices[row]
+        } else {
+            return deviceTypes[row]
+        }
+        
     }
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         // Update the selected device variable
-        selectedDevice = devices[row]
+        if pickerView == devicePicker {
+            selectedDevice = devices[row]
+        } else {
+            selectedDeviceType = deviceTypes[row]
+        }
+        
     }
     
-}
-
-extension AddSessionViewController: UITextFieldDelegate {
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        devices.append(deviceTextField.text!)
-        print(devices)
-        return true
-    }
 }
